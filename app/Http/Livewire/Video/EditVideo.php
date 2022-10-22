4fa62d1ae2f0a790b4d0dc37c1bf132c1e4fb114
theJohnCode <ifeanyichukwujohn70@gmail.com>
2022-10-22 23:01:00 +0100

    added video edit form

--- app/Http/Livewire/Video/EditVideo.php
+++ app/Http/Livewire/Video/EditVideo.php
@@ -2,13 +2,45 @@
 
 namespace App\Http\Livewire\Video;
 
+use App\Models\Channel;
+use App\Models\Video;
 use Livewire\Component;
 
 class EditVideo extends Component
 {
+    public Channel $channel;
+    public Video $video;
+
+    protected $rules = [
+        'video.title' => 'required|max:255',
+        'video.description' => 'nullable|max:1000',
+        'video.visibility' => 'nullable|in:private,public,unlisted',
+    ];
+
+    public function mount($channel, $video)
+    {
+        $this->channel = $channel;
+        $this->video = $video;
+    }
+
     public function render()
     {
         return view('livewire.video.edit-video')
-        ->extends('layouts.master');
+            ->extends('layouts.master');
+    }
+
+    public function update()
+    {
+        $this->validate();
+
+        $this->video->update([
+            'title' => $this->video->title,
+            'description' => $this->video->description,
+            'visibility' => $this->video->visibility,
+        ]);
+
+        toast('Video updated successfully','success');
+
+        return redirect()->route('home');
     }
 }
--- resources/views/livewire/video/edit-video.blade.php
+++ resources/views/livewire/video/edit-video.blade.php
@@ -1,3 +1,61 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div class="center" style="margin: 0 auto; width: 70%">
+        <div class="card">
+            <div class="card-header">{{ __('Edit Video') }}</div>
+
+            <div class="card-body">
+
+                <form wire:submit.prevent='update'>
+
+                    <div class="row mb-3">
+                        <label for="title" class="col-md-4 col-form-label text-md-end">{{ __('Video Title') }}</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" wire:model='video.title'>
+                        </div>
+                        @error('video.title')
+                            <span style="display:block; width: 70%; margin:auto; background:none; border:none"
+                                class="alert alert-danger text-center">
+                                <strong>{{ $message }}</strong>
+                            </span pan>
+                        @enderror
+                    </div>
+
+                    <div class="row mb-3">
+                        <label for="description"
+                            class="col-md-4 col-form-label text-md-end">{{ __('Video Description') }}</label>
+
+                        <div class="col-md-6">
+                            <textarea style="resize: none" cols="30" rows="4" class="form-control" wire:model='video.description'></textarea>
+                        </div>
+                    </div>
+                    @error('video.description')
+                        <span style="display:block; width: 70%; margin:auto; background:none; border:none"
+                            class="alert alert-danger text-center">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+
+                    <div class="row mb-3">
+                        <label for="visibility" class="col-md-4 col-form-label text-md-end">{{ __('Privacy') }}</label>
+
+                        <div class="col-md-6">
+                            <select wire:model="video.visibility" class="form-control">
+                                <option value="private">Private</option>
+                                <option value="public">Public</option>
+                                <option value="unlisted">Unlisted</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                Update
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
